4653977d0ab8fcd79fddc3ee101c4aeefae51dd3
360matt <mattbacq2004@gmail.com>
2021-08-29 17:13:03 +0200

    Renammed "length" by "cursor"

--- src/main/java/fr.i360matt.textEffects/Animation.java
+++ src/main/java/fr.i360matt.textEffects/Animation.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Animation {
-    public int lenght = 0;
+    public int cursor = 0;
     public List<String> formatted = new ArrayList<>();
 
     public final String getNext () {
-        return (lenght < formatted.size())
-                ? formatted.get(lenght++)
-                : formatted.get( (lenght=0) );
+        return (cursor < formatted.size())
+                ? formatted.get(cursor++)
+                : formatted.get( (cursor=0) );
     }
 
 }
